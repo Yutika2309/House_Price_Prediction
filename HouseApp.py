53b7daba5bd02bb117fc12b91f433328812d514b
Yutika Rege <61589893+Yutika2309@users.noreply.github.com>
2021-04-28 12:16:47 +0530

    Add files via upload

---
+++ HouseApp.py
@@ -0,0 +1,44 @@
+import streamlit as st
+import numpy as np
+import pickle
+
+model = pickle.load(open('model.pkl','rb'))
+
+list_bhk_bath = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+
+def predict_price(total_sqft,bath,BHK,price_per_sqft):
+    
+    input=np.array([[total_sqft,bath,BHK,price_per_sqft]]).astype(np.float64)
+    
+    prediction=model.predict(input)
+    return float(prediction)
+
+page_bg_img = '''
+<style>
+body {
+background-image: url("https://wallpaperaccess.com/full/636869.png");
+background-size: cover;
+
+}
+</style>
+'''
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
+def main():
+    st.markdown("<h1 style='text-align: center; color: black;'>House Price Prediction</h1>", unsafe_allow_html=True)
+    st.markdown("## Total Area (sq.ft.) : ")
+    total_sqft = st.text_input("")
+    st.markdown("## No. of bedrooms:  ")
+    BHK = st.select_slider(" ", options=list_bhk_bath, key = "<uniquevalueofsomesort>")
+    st.markdown("## No. of bathrooms:  ")
+    bath = st.select_slider(" ", options=list_bhk_bath)
+    st.markdown("## Price per sq.ft. : ")
+    price_per_sqft = st.text_input(" ")
+    
+
+    if st.button("Predict"):
+        output=predict_price(total_sqft,bath,BHK,price_per_sqft)
+        st.success(round(output))
+    
+if __name__=='__main__':
+    main()
